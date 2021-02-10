6a4a9f6a92445d4e1b12431f719b03b0a2ef0466
Diego Obando <diegoobando20@gmail.com>
2021-02-09 23:31:09 -0300

    Gson lib added :coffee:

--- src/BlockchainJava.java
+++ src/BlockchainJava.java
@@ -1,13 +1,18 @@
+import java.util.ArrayList;
+
+import com.google.gson.GsonBuilder;
+
 public class BlockchainJava {
 
+    public static ArrayList<Block> blockchain = new ArrayList<Block>();
+
     public static void main(String[] args) {
-        Block genesisBlock = new Block("Genesis block", "0");
-        System.out.println("Hash del bloque 1 : " + genesisBlock.hash);
+        // Add our blocks to the blockchain ArrayList:
+        blockchain.add(new Block("Genesis Block", "0"));
+        blockchain.add(new Block("Segundo bloque", blockchain.get(blockchain.size() - 1).hash));
+        blockchain.add(new Block("Tercer bloque", blockchain.get(blockchain.size() - 1).hash));
 
-        Block secondBlock = new Block("Segundo bloque", genesisBlock.hash);
-        System.out.println("Hash del bloque 2 : " + secondBlock.hash);
-
-        Block thirdBlock = new Block("Tercer bloque", secondBlock.hash);
-        System.out.println("Hash del bloque 3 : " + thirdBlock.hash);
+        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println(blockchainJson);
     }
 }
